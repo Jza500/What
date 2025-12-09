ea86ee968a5c5393f0eab1eed8e7313f663ccd4e
J_ <kittiphob_i@cmu.ac.th>
2025-12-09 16:24:22 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int Even=0;
+    int Odd=0;
+    int dum;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> dum;
+    while(dum!=0)
+    {
+        if (dum%2==0){
+            Even++;
+        }else{
+            Odd++;
+        }
+    cout << "Enter an integer: ";
+    cin >> dum;
+    }
+    cout << "#Even numbers = " << Even << endl;
+    cout << "#Odd numbers = " << Odd;
 }
